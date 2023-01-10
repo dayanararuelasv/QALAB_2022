b5b21532d033ea9b78cb8dc74a7b050e7b967ac9
gdayanara <dayanara.ruelas.v@gmail.com>
2023-01-09 21:41:09 -0500

    Se agrego comentario

--- POO_Game/src/Main.java
+++ POO_Game/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-
+        // EDITADO POR GIANELLA DAYANARA
         Toyota toyo = new Toyota("PAS201"); //200
         toyo.encenderMotor();
         toyo.avanzar();
